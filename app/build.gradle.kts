92122379b045d2e6a244663e819798d70f728ee1
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-10-31 19:20:38 +0530

    Targeted SDK changed to 30 back again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,13 @@ plugins {
 }
 
 android {
-    compileSdkVersion(31)
+    compileSdkVersion(30)
     buildToolsVersion("30.0.3")
 
     defaultConfig {
         applicationId = "com.example.news"
         minSdkVersion(23)
-        targetSdkVersion(31)
+        targetSdkVersion(30)
         versionCode(1)
         versionName("1.0")
 
